7e0ba802d1773c080940d632fb379b19427cfe9b
xiyang <evencoder@163.com>
2016-03-10 16:43:28 +0800

    登录注册设置
    登录注册设置

--- MM/MMLoginViewController.m
+++ MM/MMLoginViewController.m
@@ -22,8 +22,10 @@
 @property (weak, nonatomic) IBOutlet UIView *loginLineTwo;
 @property (weak, nonatomic) IBOutlet UIView *registerLineOne;
 @property (weak, nonatomic) IBOutlet UIView *registerLineTwo;
-
-
+/** 右边约束 */
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *rightSpace;
+/** 左边约束 */
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leftSpace;
 
 
 
@@ -35,11 +37,72 @@
     [super viewDidLoad];
     
     NSMutableDictionary *attr = [NSMutableDictionary dictionary];
-    attr[NSForegroundColorAttributeName] = MMboldColor;
-    attr[NSFontAttributeName] = [UIFont systemFontOfSize:15];
+    attr[NSForegroundColorAttributeName] = [UIColor blackColor];
+    attr[NSFontAttributeName] = [UIFont systemFontOfSize:18];
     [self.navigationController.navigationBar setTitleTextAttributes:attr];
     
+    [self.txtLoginAccount addTarget:self action:@selector(accountTextFieldDidBeginEditing) forControlEvents:UIControlEventEditingDidBegin];
+    [self.txtLoginAccount addTarget:self action:@selector(accountTextfieldDidEndEditing) forControlEvents:UIControlEventEditingDidEnd];
+    [self.txtLoginPassword addTarget:self action:@selector(passwordTextFieldDidBeginEditing) forControlEvents:UIControlEventEditingDidBegin];
+    [self.txtLoginPassword addTarget:self action:@selector(passwordTextFieldDidEndEditing) forControlEvents:UIControlEventEditingDidEnd];
+    [self.txtRegisterAccount addTarget:self action:@selector(accountTextFieldDidBeginEditing) forControlEvents:UIControlEventEditingDidBegin];
+    [self.txtRegisterAccount addTarget:self action:@selector(accountTextfieldDidEndEditing) forControlEvents:UIControlEventEditingDidEnd];
+    [self.txtRegisterPassword addTarget:self action:@selector(passwordTextFieldDidBeginEditing) forControlEvents:UIControlEventEditingDidBegin];
+    [self.txtRegisterPassword addTarget:self action:@selector(passwordTextFieldDidEndEditing) forControlEvents:UIControlEventEditingDidEnd];
 }
+
+#pragma mark - 改变TextField下划线颜色方法
+- (void)accountTextFieldDidBeginEditing {
+    
+    self.loginLineOne.backgroundColor = MMTintColor;
+    self.registerLineOne.backgroundColor = MMTintColor;
+}
+
+- (void)accountTextfieldDidEndEditing {
+    
+    self.loginLineOne.backgroundColor = MMboldColor;
+    self.registerLineOne.backgroundColor = MMboldColor;
+}
+
+- (void)passwordTextFieldDidBeginEditing {
+    
+    self.loginLineTwo.backgroundColor = MMTintColor;
+    self.registerLineTwo.backgroundColor = MMTintColor;
+}
+
+- (void)passwordTextFieldDidEndEditing {
+    
+    self.loginLineTwo.backgroundColor = MMboldColor;
+    self.registerLineTwo.backgroundColor = MMboldColor;
+}
+
+#pragma mark - 切换注册和登录界面
+- (IBAction)registerAccountClict:(UIButton *)sender {
+    
+    self.leftSpace.constant = - MMWidth;
+    self.rightSpace.constant = MMWidth - MMConstantMargin;
+    [UIView animateWithDuration:0.25 animations:^{
+        
+        [self.view layoutIfNeeded]; // 刷新约束
+    }];
+    // 修改导航栏名
+    self.title = @"注册MM";
+}
+
+- (IBAction)loginAccountClick:(UIButton *)sender {
+    
+    self.leftSpace.constant = - MMConstantMargin;
+    self.rightSpace.constant = - MMConstantMargin;
+    [UIView animateWithDuration:0.25 animations:^{
+        
+        [self.view layoutIfNeeded]; // 刷新约束
+    }];
+    self.title = @"登录MM";
+}
+
+
+
+
 
 
 - (IBAction)loginClick:(id)sender {
